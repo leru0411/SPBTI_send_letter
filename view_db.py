9f22fdcde508abcdf3401abad745ce22bea5c59c
Valeriya <leru0411@mail.ru>
2026-01-23 23:06:29 +0300

    убраны лишние комментарии

--- view_db.py
+++ view_db.py
@@ -13,25 +13,6 @@ rows = cursor.fetchall()
 columns = [desc[0] for desc in cursor.description]  # имена столбцов
 print(tabulate(rows, headers=columns, tablefmt="grid"))
 
-# tables = [t[0] for t in cursor.fetchall()]
-
-# if not tables:
-#     print("В базе данных нет таблиц")
-# else:
-#     for table in tables:
-#         print(f"\n===== Таблица: {table} =====")
-#         # Получаем названия столбцов
-#         cursor.execute(f"PRAGMA table_info({table});")
-#         columns = [info[1] for info in cursor.fetchall()]
-#         # Получаем все данные
-#         cursor.execute(f"SELECT * FROM {table};")
-#         rows = cursor.fetchall()
-#         # Выводим красиво через tabulate
-#         if rows:
-#             print(tabulate(rows, headers=columns, tablefmt="grid"))
-#         else:
-#             print("(Таблица пуста)")
-
 
 
 conn.close()
